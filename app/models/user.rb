f04d2f3150bac3a800db81be5e3d1821f3b07cf5
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-01-27 14:30:25 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,16 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :date_of_birth, presence: true
 
-  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters'} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } do
     validates :last_name
     validates :first_name
   end
 
-  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: 'Full-width characters'} do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width characters' } do
     validates :last_name_kana
     validates :first_name_kana
   end
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'include both letters and numbers'
 end
