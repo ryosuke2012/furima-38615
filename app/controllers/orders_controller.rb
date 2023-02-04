1666bb628778e91d37daa0c32fa12aaca833a403
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-02-04 18:48:04 +0900

    ordersコントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
 
   def index
     @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new
+    if @item.user == current_user
+      redirect_to root_path
+    end
   end
 end
