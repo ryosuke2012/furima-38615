e28f5d87b5cd955104a34c2512963d631bf91a40
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-02-04 14:48:37 +0900

    データをテーブルに保存する処理を書く

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -11,4 +11,9 @@ class OrderDelivery
     validates :item_id
   end
   validates :region_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Delivery.create(postcode: postcode, region_id: region_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 end
