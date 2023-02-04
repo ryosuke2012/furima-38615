302350b2ec8934d7017f9e7f2ada6c664b340c3d
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-02-04 16:05:45 +0900

    購入のためのフォームの実装とcssファイルの修正

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -6,7 +6,7 @@ class OrderDelivery
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipality
     validates :house_number
-    validates :phone_number, format: {/\A\d{10,11}\z/, message: 'is invalid'}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid"}
     validates :user_id
     validates :item_id
   end
