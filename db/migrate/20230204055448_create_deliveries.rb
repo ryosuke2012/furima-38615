f89168052a5fa88e1d0b67250a40830994c0af5a
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-02-04 15:06:41 +0900

    マイグレーションを実行

--- db/migrate/20230204055448_create_deliveries.rb
+++ db/migrate/20230204055448_create_deliveries.rb
@@ -1,7 +1,13 @@
 class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
     create_table :deliveries do |t|
-
+      t.string :postcode, null: false
+      t.integer :region_id, null: false
+      t.string :municipality, null: false
+      t.string :house_number, null: false
+      t.string :building_name
+      t.string :phone_number, null: false
+      t.references :order, null: false, foreign_key: true
       t.timestamps
     end
   end
