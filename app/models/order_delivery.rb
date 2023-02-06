ce3fbb6b9c037ed0e2b99b6251d86022611a2b64
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-02-06 15:02:14 +0900

    トークンに対するバリデーションを設定

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -9,6 +9,7 @@ class OrderDelivery
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid"}
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :region_id, numericality: {other_than: 1, message: "can't be blank"}
 
