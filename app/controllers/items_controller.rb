7bfcedf2c9a40b9649aa2ea367efcba020ad031f
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-02-15 12:35:12 +0900

    ストロングパラメーターを編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,8 +42,8 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :region_id,
-                                 :delivery_time_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :region_id,
+                                 :delivery_time_id, :price, {images: []}).merge(user_id: current_user.id)
   end
 
   def set_item
