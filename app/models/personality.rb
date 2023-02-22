cd811323c74ff34973ce1c64110848103cc1c412
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-02-22 18:27:30 +0900

    AddressモデルとUserモデルを編集

--- app/models/personality.rb
+++ app/models/personality.rb
@@ -1,2 +1,15 @@
 class Personality < ApplicationRecord
+  belongs_to :user, optional: true
+
+  validates :date_of_birth, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'を全角文字で入力してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'を全角文字で入力してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'に英数字を含めてください'
 
   has_many :items
   has_many :orders
