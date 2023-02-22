ffc5b93481695f64ad10fa91bb84b1edbd8beb14
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-02-22 18:51:15 +0900

    ルーティングを編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'personalities', to: 'users/registrations#new_personality'
+    post 'personalities', to: 'users/registrations#create_personality'
+  end
   root to: "items#index"
   resources :items do
     resources :orders, only: [:index, :create]
