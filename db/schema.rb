8b1ced3da72a77b0e25222c65865ff1c5861cddc
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-02-22 18:09:20 +0900

    マイグレーションファイルを編集

--- db/schema.rb
+++ db/schema.rb
@@ -99,11 +99,6 @@ ActiveRecord::Schema.define(version: 2023_02_04_055448) do
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.date "date_of_birth", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
