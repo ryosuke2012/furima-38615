8dec0c46628fd9f15794b5175e33c7d44f94e9ce
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-03-03 11:46:25 +0900

    バリデーションの処理を書く

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,27 +3,4 @@ class Item < ApplicationRecord
   has_one :order
   has_many_attached :images
   has_many :comments
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :shipping_fee
-  belongs_to :region
-  belongs_to :delivery_time
-
-  validates :name, :description, :images, presence: true
-  validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
-
-  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                      presence: { message: "を入力してください" }
-  end
-
-  with_options numericality: { other_than: 1, message: "を選択してください" } do
-    validates :category_id
-    validates :condition_id
-    validates :shipping_fee_id
-    validates :region_id
-    validates :delivery_time_id
-  end
 end
