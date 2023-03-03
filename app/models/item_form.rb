618dec3bb62bef2b58b379e8dd8d13b07276b68c
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-03-03 11:43:53 +0900

    保存したいカラム名を属性値として扱えるようにする

--- app/models/item_form.rb
+++ app/models/item_form.rb
@@ -1,3 +1,5 @@
 class ItemForm
   include ActiveModel::Model
+
+  attr_accessor :name, :description, :images, :price, :category_id, :condition_id, :shipping_fee_id, :region_id, :delivery_time_id
 end
