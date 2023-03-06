4a89c53c7dac26c9ab45fb91d2c1f126c9dd8dae
ryosuke2012 <luckywater7@yahoo.co.jp>
2023-03-06 15:39:46 +0900

    新規投稿画面と編集画面で動く関数を用意

---
+++ app/javascript/tag.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const tagNameInput = document.getElementById("item-tag");
+    if (tagNameInput){
+      console.log("読み込み完了");
+    };
+  });
